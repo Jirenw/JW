758ff4cf3c3483381cbc580d6319c9aa25f01ba6
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-29 12:24:54 -0500

    Update nsfwmenu.js

--- src/nsfwmenu.js
+++ src/nsfwmenu.js
@@ -1,17 +1,15 @@
 const nsfwmenu = (prefix, pushname) => {
-    return `*Comandos para ver pornito 🔞*
+    return `*COMANDOS +18-🔞*
+
 *NSFW ✅*
 
-- ${prefix}nsfwbobs
-- ${prefix}nsfwsidebobs
-- ${prefix}nsfwahegao
-- ${prefix}nsfwfeets
+᚛ ${prefix}nsfwbobs
+᚛ ${prefix}nsfwsidebobs
+᚛ ${prefix}nsfwahegao
+᚛ ${prefix}nsfwfeets
 
-_El bot nesecita admin y tener activado los NSFW_\n _Digita_\n ${prefix}*nsfw 1*
-
-Algunas funciones fueron eliminas por errores en el servidor de la India
-
-ву ѕнαη∂υу`
+_El bot necesita admin y tener activado los NSFW_\n _Digita_\n ${prefix}*nsfw 1*
+By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽`
 
 }
 
