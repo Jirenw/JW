5d9727d266c456c303c1368a4fd24e6ec538552d
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-29 12:09:52 -0500

    Update antimenu.js

--- src/antimenu.js
+++ src/antimenu.js
@@ -1,76 +1,65 @@
 const antimenu = (prefix, pushname) => {
     return `
+*COMANDOS DE ANTILINK'S📢*
+━━━━━━━━━━━━━━━━━━━━ 
+_*[][Antilink De Grupos De Whatsapp][]*_
 
-*Comandos Para Activar Los Antilinks 📢*
-
-══════════════
-
-_*Antilink De Grupos De Whatsapp*_
-
-*PARA ACTIVAR EL ANTILINK*
+*✓PARA ACTIVAR EL ANTILINK*
 ${prefix}antilink 1
 
-*PARA DESACTIVAR EL ANTILINK*
+*✓PARA DESACTIVAR EL ANTILINK*
 ${prefix}antilink 0
+━━━━━━━━━━━━━━━━━━━━ 
+_*[][Antilink De Instagram][]*_
 
-══════════════
-
-_*Antilink De Instagram*_
-
-*PARA ACTIVAR EL ANTILINK*
+*✓PARA ACTIVAR EL ANTILINK*
 ${prefix}antinsta 1
 
-*PARA DESACTIVAR EL ANTILINK*
+*✓PARA DESACTIVAR EL ANTILINK*
 ${prefix}antinsta 0
+━━━━━━━━━━━━━━━━━━━━ 
+_*[][Antilink De Facebook][]*_
 
-══════════════
-
-_*Antilink De Facebook*_
-
-*PARA ACTIVAR EL ANTILINK*
+*✓PARA ACTIVAR EL ANTILINK*
 ${prefix}antiface 1
 
-*PARA DESACTIVAR EL ANTILINK*
+*✓PARA DESACTIVAR EL ANTILINK*
 ${prefix}antiface 0
+━━━━━━━━━━━━━━━━━━━━ 
+_*[][Antilink De TikTok][]*_
 
-══════════════
-
-_*Antilink De Tik Tok*_
-
-*PARA ACTIVAR EL ANTILINK*
+*✓PARA ACTIVAR EL ANTILINK*
 ${prefix}antitik 1
 
-*PARA DESACTIVAR EL ANTILINK*
+*✓PARA DESACTIVAR EL ANTILINK*
 ${prefix}antitik 0
+━━━━━━━━━━━━━━━━━━━━ 
+_*[][Antilink De Youtube][]*_
 
-══════════════
+*✓PARA ACTIVAR EL ANTILINK*
+${prefix}antitube 1
 
+*✓PARA DESACTIVAR EL ANTILINK*
+${prefix}antitube 0
+━━━━━━━━━━━━━━━━━━━━ 
+_*[][Antilink De Kwai][]*_
 
-_*Antilink De Kwai*_
-
-*PARA ACTIVAR EL ANTILINK*
+*✓PARA ACTIVAR EL ANTILINK*
 ${prefix}antikwai 1
 
-*PARA DESACTIVAR EL ANTILINK*
+*✓PARA DESACTIVAR EL ANTILINK*
 ${prefix}antikwai 0
- 
-══════════════
-
-
-_*Antilink De Discord*_
+━━━━━━━━━━━━━━━━━━━━
+_*[][Antilink De Discord][]*_
 
 *PARA ACTIVAR EL ANTILINK*
 ${prefix}antidiscord 1
 
 *PARA DESACTIVAR EL ANTILINK*
 ${prefix}antidiscord 0
- 
-══════════════
-
+━━━━━━━━━━━━━━━━━━━━ 
 _Para activar estos comandos el bot necesita tener admin en el grupo y esta funcion solo la activan los admins del grupo_
-
-
-ву ѕнαη∂υу
+*BY.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽*
 `
 
 }
