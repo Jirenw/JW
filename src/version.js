d48401ad7aaefc0e1a0abca13a8ce2be3a1b1668
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-29 12:51:31 -0500

    Update version.js

--- src/version.js
+++ src/version.js
@@ -2,17 +2,17 @@ const version = (prefix, pushname) => {
     return `
 *┍━━━━╝✹VERSION DEL BOT✹╚━━━━┑*
 
-*Actualizado:* 29 de Agosto del 2021
-*Versión actual:* 3.8
-*Ofrecido por:*JIREN Y WENDY
-
-*INFORME*
-
-ELLA NO TE AMO/NO TE AMA/NI TE AMARA
-
-*COMO ACTUALIZAR*
-
-Comuníquese con Jiren ._. <> http://wa.me/+524591347471
+┏━━━━━━━━━━━━━━━━━━━━ 
+┣☛ *Actualizado:* 29 de Agosto del 2021
+┣☛ *Versión actual:* 3.8
+┣☛ *Ofrecido por:*JIREN Y WENDY
+┣━━━━━━━━━━━━━━━━━━━━ 
+┣☛ *INFORME*
+┃ ELLA NO TE AMO/NO TE AMA/NI TE AMARA
+┣━━━━━━━━━━━━━━━━━━━━
+┣☛ *COMO ACTUALIZAR*
+┃Comuníquese con Jiren ._. <> http://wa.me/+524591347471
+╰━━━━━━━━━━━━━━━━━━━━
 
 BY.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽
 
