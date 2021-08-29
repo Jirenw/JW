40c6a033522365350493309cb07f0c13e2b6b2de
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-29 14:00:32 -0500

    Update welmenu.js

--- src/welmenu.js
+++ src/welmenu.js
@@ -3,10 +3,10 @@ const welmenu = (prefix, pushname) => {
 *┍━━━━╝✹BIENVENIDA✹╚━━━━┑*
 
 ┏━━━━━━━━━━━━━━━━━━━━ 
-┣ $ { prefix } bienvenido 1
+┣ ${ prefix } bienvenido 1
 ┃ *PARA ACTIVAR LA BIENVENIDA *
 ┣━━━━━━━━━━━━━━━━━━━━ 
-┣ $ { prefix } bienvenido 0
+┣ ${ prefix } bienvenido 0
 ┃ *PARA DESACTIVAR LA BIENVENIDA *
 ╰━━━━━━━━━━━━━━━━━━━━
 _Para activar este comando el bot tiene que tener admin_
