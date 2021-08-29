c0c8f9f46e59e56ba008a573e9616e056892fe72
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-29 12:19:53 -0500

    Update kickmenu.js

--- src/kickmenu.js
+++ src/kickmenu.js
@@ -1,28 +1,21 @@
 const kickmenu = (prefix, pushname) => {
-    return `*Comando De Eliminar 📤*
-
-_Para usar esta funcion el bot necesita admin_
-
-══════════════
-
+    return `*ELIMINAR PARTICIPANTE DEL GRUPO📤*
+━━━━━━━━━━━━━━━━━━━━ 
 *COMANDO*
 
 *kick + la personas que deseas eliminar
 
 Ejemplo: *kick @xxxxxx
-
-══════════════
-
+━━━━━━━━━━━━━━━━━━━━ 
 *⚠ADVERTENCIA⚠*
 
-NUNCA COLOCAR MAS DE DOS PERSONAS PARA QUE LAS ELIMINE
+NUNCA COLOCAR MAS DE DOS PERSONAS PARA QUE LAS ELIMINE.
 
 Ejemplo: *kick @xxxxxx @xxxxx
 
-Ya que el numero del bot se ira a soporte
-Quedas advertido :)
+Ya que el numero del bot se ira a soporte.
 
-ву ѕнαη∂υу`
+*BY.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽*`
 
 }
 
