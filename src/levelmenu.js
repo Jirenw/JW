4594a2458f4426b203b1c5570bc0cd5f617992d2
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-29 12:21:09 -0500

    Update levelmenu.js

--- src/levelmenu.js
+++ src/levelmenu.js
@@ -1,32 +1,20 @@
 const levelmenu = (prefix, pushname) => {
     return `
-
-*Comandos De Level 🆕 beta*
+*COMANDOS DE NIVEL UwU*
  
-══════════════
+━━━━━━━━━━━━━━━━━━━━ 
+PARA ACTIVARLO EL BOT NECESITA SER ADMIN.
 
-PARA ACTIVARLO EL BOT NECESITA DE ADMINISTRACIÓN
-
-*PARA ACTIVAR LEVEL*
-
+*PARA ACTIVAR NIVEL*
 *leveling 1
 
-
-*PARA DESACTIVAR LEVEL*
-
+*PARA DESACTIVAR NIVEL*
 *leveling 0
-
-══════════════
-
-PARA VER TU XP EN TIEMPO REAL
-
+━━━━━━━━━━━━━━━━━━━━ 
+PARA VER TU NIVEL EN TIEMPO REAL
 *level
-
-══════════════
     
-*Recuerda que si actualizas tu bot o borras el numero del bot o otra cosa todos los levels de los particiantes quedaran en 0*
-    
-ву ѕнαη∂υу
+By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽
 `
 
 }
