8c82e34059a3e40917b3c47972af8798fd26ed5c
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-29 13:21:49 -0500

    Update antimenu.js

--- src/antimenu.js
+++ src/antimenu.js
@@ -2,64 +2,52 @@ const antimenu = (prefix, pushname) => {
     return `
 *┍━━━━╝✹ANTILINK'S🗝️✹╚━━━━┑*
 
-━━━━━━━━━━━━━━━━━━━━ 
-_*[][Antilink De Grupos De Whatsapp][]*_
+┏━━━━━━━━━━━━━━━━━━━━ 
+┣ _*[][Antilink De Grupos De Whatsapp][]*_
+┃ _PARA ACTIVAR EL ANTILINK_
+┣☛ ${prefix}antilink 1
+┃ _PARA DESACTIVAR EL ANTILINK_
+┣☛ ${prefix}antilink 0
+┣━━━━━━━━━━━━━━━━━━━━ 
+┣ _*[][Antilink De Instagram][]*_
+┃ _PARA ACTIVAR EL ANTILINK_
+┣☛ ${prefix}antinsta 1
+┃ _PARA DESACTIVAR EL ANTILINK_
+┣☛ ${prefix}antinsta 0
+┣━━━━━━━━━━━━━━━━━━━━ 
+┣ _*[][Antilink De Facebook][]*_
+┃ _PARA ACTIVAR EL ANTILINK_
+┣☛ ${prefix}antiface 1
+┃ _PARA DESACTIVAR EL ANTILINK_
+┣☛ ${prefix}antiface 0
+┣━━━━━━━━━━━━━━━━━━━━ 
+┣ _*[][Antilink De TikTok][]*_
+┃ _PARA ACTIVAR EL ANTILINK_
+┣☛ ${prefix}antitik 1
+┃ _PARA DESACTIVAR EL ANTILINK_
+┣☛ ${prefix}antitik 0
+┣━━━━━━━━━━━━━━━━━━━━ 
+┣ _*[][Antilink De Youtube][]*_
+┃ _PARA ACTIVAR EL ANTILINK_
+┃☛ ${prefix}antitube 1
+┣ _PARA DESACTIVAR EL ANTILINK_
+┣☛ ${prefix}antitube 0
+┣━━━━━━━━━━━━━━━━━━━━ 
+┣ _*[][Antilink De Kwai][]*_
+┃ _PARA ACTIVAR EL ANTILINK_
+┣☛ ${prefix}antikwai 1
+┃ _PARA DESACTIVAR EL ANTILINK_
+┣☛ ${prefix}antikwai 0
+┣━━━━━━━━━━━━━━━━━━━━ 
+┣ _*[][Antilink De Discord][]*_
+┃ _PARA ACTIVAR EL ANTILINK_
+┣☛ ${prefix}antidiscord 1
+┃ _PARA DESACTIVAR EL ANTILINK_
+┣☛ ${prefix}antidiscord 0
+╰━━━━━━━━━━━━━━━━━━━━
 
-*✓PARA ACTIVAR EL ANTILINK*
-${prefix}antilink 1
-
-*✓PARA DESACTIVAR EL ANTILINK*
-${prefix}antilink 0
-━━━━━━━━━━━━━━━━━━━━ 
-_*[][Antilink De Instagram][]*_
-
-*✓PARA ACTIVAR EL ANTILINK*
-${prefix}antinsta 1
-
-*✓PARA DESACTIVAR EL ANTILINK*
-${prefix}antinsta 0
-━━━━━━━━━━━━━━━━━━━━ 
-_*[][Antilink De Facebook][]*_
-
-*✓PARA ACTIVAR EL ANTILINK*
-${prefix}antiface 1
-
-*✓PARA DESACTIVAR EL ANTILINK*
-${prefix}antiface 0
-━━━━━━━━━━━━━━━━━━━━ 
-_*[][Antilink De TikTok][]*_
-
-*✓PARA ACTIVAR EL ANTILINK*
-${prefix}antitik 1
-
-*✓PARA DESACTIVAR EL ANTILINK*
-${prefix}antitik 0
-━━━━━━━━━━━━━━━━━━━━ 
-_*[][Antilink De Youtube][]*_
-
-*✓PARA ACTIVAR EL ANTILINK*
-${prefix}antitube 1
-
-*✓PARA DESACTIVAR EL ANTILINK*
-${prefix}antitube 0
-━━━━━━━━━━━━━━━━━━━━ 
-_*[][Antilink De Kwai][]*_
-
-*✓PARA ACTIVAR EL ANTILINK*
-${prefix}antikwai 1
-
-*✓PARA DESACTIVAR EL ANTILINK*
-${prefix}antikwai 0
-━━━━━━━━━━━━━━━━━━━━
-_*[][Antilink De Discord][]*_
-
-*PARA ACTIVAR EL ANTILINK*
-${prefix}antidiscord 1
-
-*PARA DESACTIVAR EL ANTILINK*
-${prefix}antidiscord 0
-━━━━━━━━━━━━━━━━━━━━ 
 _Para activar estos comandos el bot necesita tener admin en el grupo y esta funcion solo la activan los admins del grupo_
+
 *BY.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽*
 `
 
