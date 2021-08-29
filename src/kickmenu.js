bb11b0b7cccb8e64107eff888ef4245ba3ac17ed
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-29 13:04:19 -0500

    Update kickmenu.js

--- src/kickmenu.js
+++ src/kickmenu.js
@@ -1,20 +1,16 @@
 const kickmenu = (prefix, pushname) => {
     return `*┍━━━━╝✹BAN-GRUPO✹╚━━━━┑*
 
-━━━━━━━━━━━━━━━━━━━━ 
-*COMANDO*
-
-*kick + la personas que deseas eliminar
-
-Ejemplo: *kick @xxxxxx
-━━━━━━━━━━━━━━━━━━━━ 
-*⚠ADVERTENCIA⚠*
-
-NUNCA COLOCAR MAS DE DOS PERSONAS PARA QUE LAS ELIMINE.
-
-Ejemplo: *kick @xxxxxx @xxxxx
-
-Ya que el numero del bot se ira a soporte.
+┏━━━━━━━━━━━━━━━━━━━━ 
+┣*COMANDO*
+┣☛ *kick + la personas que deseas eliminar
+┣Ejemplo: *kick @xxxxxx
+┣━━━━━━━━━━━━━━━━━━━━ 
+┣*⚠ADVERTENCIA⚠*
+┣☛NUNCA COLOCAR MAS DE DOS PERSONAS PARA QUE LAS ELIMINE.
+┣Ejemplo: *kick @xxxxxx @xxxxx
+┃Ya que el numero del bot se ira a soporte.
+╰━━━━━━━━━━━━━━━━━━━━
 
 *BY.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽*`
 
