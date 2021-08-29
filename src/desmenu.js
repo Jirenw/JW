457d0d2b1cd33f4bda76972f6f0c38e6d2545687
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-29 13:09:15 -0500

    Update desmenu.js

--- src/desmenu.js
+++ src/desmenu.js
@@ -2,23 +2,24 @@ const desmenu = (prefix, pushname) => {
     return `
 *┍━━━━╝✹DESCARGAS✹╚━━━━┑*
 
-━━━━━━━━━━━━━━━━━━━━ 
-*🎵AUDIO🎶*
-${prefix}play2
-Una alternativa por si *play llego a su limite de canciones.
-${prefix}play
-Coloca el nombre de la cancion, o el link de YouTube.
-_Ejm_
-${prefix}play Faded
-━━━━━━━━━━━━━━━━━━━━ 
-*📽️VIDEO📹*
-${prefix}ytmp4
-Descarga videos de YouTube
-_Ejm_
-${prefix}ytmp4 www.youtube.com/xxxxxxxx
-━━━━━━━━━━━━━━━━━━━━
-_*play y *play2 tiene un limite de 2000 canciones, y se restablece cada 24 horas_
-     
+┏━━━━━━━━━━━━━━━━━━━━ 
+┣*🎵AUDIO🎶*
+┣☛ ${prefix}play2
+┃Una alternativa por si *play llego a su limite de canciones.
+┣☛ ${prefix}play
+┃Coloca el nombre de la cancion, o el link de YouTube.
+┃_Ejm_
+┣☛ ${prefix}play Faded
+┣━━━━━━━━━━━━━━━━━━━━ 
+┣*📽️VIDEO📹*
+┣☛ ${prefix}ytmp4
+┃Descarga videos de YouTube
+┃_Ejm_
+┣☛ ${prefix}ytmp4 www.youtube.com/xxxxxxxx
+┣━━━━━━━━━━━━━━━━━━━━ 
+┃_*play y *play2 tiene un limite de 2000 canciones, y se restablece cada 24 horas_
+╰━━━━━━━━━━━━━━━━━━━━   
+
 BY.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽`
 
 }
