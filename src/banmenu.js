112a5914aac56fe4df761509f1a5821e4d9e55d1
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-29 13:12:40 -0500

    Update banmenu.js

--- src/banmenu.js
+++ src/banmenu.js
@@ -1,22 +1,19 @@
 const banmenu = (prefix, pushname) => {
     return `*┍━━━━╝✹BAN-BOT✹╚━━━━┑*
-    
- _👀EL BOT DEBE SER ADMIN👀_
 
-D esta manera se impide que determinados participantes usen el bot.
-━━━━━━━━━━━━━━━━━━━━ 
-*PARA BANEAR*
+┏━━━━━━━━━━━━━━━━━━━━    
+┃ _👀EL BOT DEBE SER ADMIN👀_
+┃Se impide que determinados participantes usen el bot.
+┣━━━━━━━━━━━━━━━━━━━━ 
+┣ *PARA BANEAR*
+┣☛ *ban + a la persona que deseas banear
+┃Ejemplo: *ban @xxxxxxx
+┣━━━━━━━━━━━━━━━━━━━━ 
+┣ *PARA DESBANEAR*
+┣☛ *desban + a la persona que deseas desbanear
+┃Ejemplo: *desban @xxxxxxx
+╰━━━━━━━━━━━━━━━━━━━━
 
-*ban + a la persona que deseas banear
-
-Ejemplo: *ban @xxxxxxx
-━━━━━━━━━━━━━━━━━━━━ 
-*PARA DESBANEAR*
-
-*desban + a la persona que deseas desbanear
-
-Ejemplo: *desban @xxxxxxx
-━━━━━━━━━━━━━━━━━━━━
 _BY.「〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽」_`
 
 }
