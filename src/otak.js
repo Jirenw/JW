fcc7dc2e7abf509f3d794461767d5b03ee33816b
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-29 12:56:29 -0500

    Update otak.js

--- src/otak.js
+++ src/otak.js
@@ -1,12 +1,13 @@
 const otak = (prefix, pushname) => {
     return `*Palabras especificas para que el bot interactue con ustedes otakus🌚*
 
-Quien es tu sempai botsito
-Me gimes 7u7
-Te amo botsito uwu
-Onichan
-La toca 7w7
-
+┏━━━━━━━━━━━━━━━━━━━━ 
+┣Quien es tu sempai botsito
+┣Me gimes 7u7
+┣Te amo botsito uwu
+┣Onichan
+┣La toca 7w7
+╰━━━━━━━━━━━━━━━━━━━━
 _*Escribe tal como esta y no te quejes del bot*_
 
 By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽`
