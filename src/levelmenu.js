3082540304988addea8fd079a909e44fd71e7d10
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-29 13:02:07 -0500

    Update levelmenu.js

--- src/levelmenu.js
+++ src/levelmenu.js
@@ -2,18 +2,19 @@ const levelmenu = (prefix, pushname) => {
     return `
 *┍━━━━╝✹NIVEL✹╚━━━━┑*
  
-━━━━━━━━━━━━━━━━━━━━ 
-PARA ACTIVARLO EL BOT NECESITA SER ADMIN.
+┏━━━━━━━━━━━━━━━━━━━━ 
+┃PARA ACTIVARLO EL BOT NECESITA SER ADMIN.
+┣━━━━━━━━━━━━━━━━━━━━
+┣*PARA ACTIVAR NIVEL*
+┣☛ *leveling 1
+┣━━━━━━━━━━━━━━━━━━━━ 
+┣*PARA DESACTIVAR NIVEL*
+┣☛ *leveling 0
+┣━━━━━━━━━━━━━━━━━━━━ 
+┣PARA VER TU NIVEL EN TIEMPO REAL
+┣☛ *level
+╰━━━━━━━━━━━━━━━━━━━━    
 
-*PARA ACTIVAR NIVEL*
-*leveling 1
-
-*PARA DESACTIVAR NIVEL*
-*leveling 0
-━━━━━━━━━━━━━━━━━━━━ 
-PARA VER TU NIVEL EN TIEMPO REAL
-*level
-    
 By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽
 `
 
