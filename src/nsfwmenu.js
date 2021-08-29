5681f7ce28c98110c682ee32f47f68cd3556ec1e
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-29 12:57:59 -0500

    Update nsfwmenu.js

--- src/nsfwmenu.js
+++ src/nsfwmenu.js
@@ -1,14 +1,15 @@
 const nsfwmenu = (prefix, pushname) => {
     return `*┍━━━━╝✹COMANDOS +18🔞✹╚━━━━┑*
 
-*NSFW ✅*
-
-᚛ ${prefix}nsfwbobs
-᚛ ${prefix}nsfwsidebobs
-᚛ ${prefix}nsfwahegao
-᚛ ${prefix}nsfwfeets
-
+┏━━━━━━━━━━━━━━━━━━━━ 
+┣*NSFW ✅*
+┣☛ ${prefix}nsfwbobs
+┣☛ ${prefix}nsfwsidebobs
+┣☛ ${prefix}nsfwahegao
+┣☛ ${prefix}nsfwfeets
+╰━━━━━━━━━━━━━━━━━━━━
 _El bot necesita admin y tener activado los NSFW_\n _Digita_\n ${prefix}*nsfw 1*
+
 By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽`
 
 }
